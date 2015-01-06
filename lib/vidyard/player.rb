e421157c07e57ba20177e22023beb74477f00d5a
nkumeiko <nkumeiko@gmail.com>
2015-01-06 11:57:24 +0700

    Allow access to all player attributes

--- lib/vidyard/player.rb
+++ lib/vidyard/player.rb
@@ -1,9 +1,10 @@
 module Vidyard
   class Player
 
-    attr_accessor :uuid, :name, :url
+    attr_accessor :uuid, :name, :url, :attrs
 
     def initialize(player)
+      @attrs = player
       @uuid = player['uuid']
       @name = player['name']
       @url = embed_url
